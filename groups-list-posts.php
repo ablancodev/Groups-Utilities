c7ab2b81a2879685d02bca93d9ffcdb1143a3338
eggemplo <eggemplo@gmail.com>
2014-02-18 09:57:09 +0100

    Create groups-list-posts.php

---
+++ groups-list-posts.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * groups-list-posts.php
+ *
+ * Copyright (c) 2013 "eggemplo" Antonio Blanco www.eggemplo.com
+ *
+ * This code is released under the GNU General Public License.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @author eggemplo
+ * @version 1.0
+ *
+ * Show the posts that current user can access.
+ * 
+ * Example:   [groups_list_posts]
+ *
+ */
+ function my_groups_list_posts() {
+
+	$posts = Groups_Post_Access::the_posts(get_posts());
+	echo '<ul>';
+	foreach ($posts as $post)  {
+		echo '<li><a href="' . get_permalink($post->ID) . '">' . get_the_title($post->ID) . '</a></li>';
+	}
+	echo '</ul>';
+	
+
+}
+add_shortcode( 'groups_list_posts', 'my_groups_list_posts' );
+?>
