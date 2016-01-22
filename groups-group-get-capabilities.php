ed496a0abf4f280d50708162b65193914d64f057
eggemplo <eggemplo@gmail.com>
2016-01-22 11:21:45 +0100

    modified

--- groups-group-get-capabilities.php
+++ groups-group-get-capabilities.php
@@ -1,14 +1,14 @@
 function groups_group_get_capabilities ( $group_id ) {
-  global $wpdb;
-  $result = false;
-  
-  $group_capability_table = _groups_get_tablename( 'group_capability' );
-  $group_capability = $wpdb->get_row( $wpdb->prepare(
-      "SELECT * FROM $group_capability_table WHERE group_id = %d",
-    Groups_Utility::id( $group_id )
-  ) );
-  if ( $group_capability !== null ) {
-    $result = $group_capability;
-  }
-  return $result;
+	global $wpdb;
+	$result = false;
+
+	$group_capability_table = _groups_get_tablename( 'group_capability' );
+	$group_capability = $wpdb->get_results( $wpdb->prepare(
+			"SELECT capability_id FROM $group_capability_table WHERE group_id = %d",
+			Groups_Utility::id( $group_id )
+	) );
+	if ( $group_capability !== null ) {
+		$result = $group_capability;
+	}
+	return $result;
 }
