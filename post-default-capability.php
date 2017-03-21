1ef43637347f75c8e4fb087171a1dd1110a3fb5b
eggemplo <eggemplo@gmail.com>
2017-03-21 09:27:24 +0100

    Update post-default-capability.php

--- post-default-capability.php
+++ post-default-capability.php
@@ -1,5 +1,6 @@
 // This code add the capability with id 160 as selected by default.
 // Change the 160 to your capability_id
+// If you don't have available the legacy mode, please use the code: https://github.com/eggemplo/Groups-Utilities/blob/master/post-default-group.php
 
 add_filter( 'groups_access_restrictions_capability_selected', 'my_groups_access_restrictions_capability_selected', 10, 6);
 function my_groups_access_restrictions_capability_selected ( 
