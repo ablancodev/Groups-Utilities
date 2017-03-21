a511a31a206eadc653276a8da71dd436d9a39b8d
eggemplo <eggemplo@gmail.com>
2017-03-21 09:27:45 +0100

    Update post-default-group.php

--- post-default-group.php
+++ post-default-group.php
@@ -1,6 +1,6 @@
 // This code add the group with id 160 as selected by default.
 // Change the 160 to your group_id
-// If you have available the legacy mode, please use the code: https://github.com/eggemplo/Groups-Utilities/blob/master/post-default-capability.php
+// If you have selected the legacy mode, please use the code: https://github.com/eggemplo/Groups-Utilities/blob/master/post-default-capability.php
 
 add_action( 'groups_access_meta_boxes_after_groups_read_update', 'my_groups_access_meta_boxes_after_groups_read_update', 10, 3);
 function my_groups_access_meta_boxes_after_groups_read_update ( $post_id, $group_ids, $update_result ) {
